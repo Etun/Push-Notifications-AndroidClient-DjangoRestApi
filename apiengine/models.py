cc712580be7ced79e3c95bdc889a17c65131e4ed
Ellen Tunik <helentunik@gmail.com>
2015-04-17 00:00:33 +0300

    final commit

--- apiengine/models.py
+++ apiengine/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class MessageModel(models.Model):
+    title = models.CharField(max_length=125)
+    text = models.TextField()
+    date = models.DateTimeField('date', auto_now=False, auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-date"]
--- tastypie_project/urls.py
+++ tastypie_project/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from apiengine.api import MessageModelResource
+
+resource_model = MessageModelResource()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tastypie_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(resource_model.urls)),
 )
